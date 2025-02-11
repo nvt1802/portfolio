d28fa42c4f99f9a36eccd3fe763627aa3fce26e2
Tai Nguyen <tai.nguyen@mgisolutions.com>
2025-02-12 00:30:44 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Portifolio from "@/components/Portifolio";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center space-y-20 py-10">
+    <main>
       <Portifolio />
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,23 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-
-  	}
+    extend: {
+      colors: {
+        malibu: "#5BADFF",
+        "regent-gray": "#8491A0",
+        white: "#FFFFFF",
+        "mine-shaft": "#2A2A2A",
+        "cod-gray": "#161513",
+        salmon: "#FF8660",
+        "electric-violet": "#9A33FF",
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
